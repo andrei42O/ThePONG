01ace1eeaadbf482cd627ef759bac4ee4be107f9
andrei42O <andrei.aciobanitei354@yahoo.com>
2021-09-10 15:36:13 +0300

    random ballin

--- engine.js
+++ engine.js
@@ -1,41 +1,74 @@
 var canvas;
 var canvasContext; 
-var ballW = 20; // 20px x 20px
-var ballH = 20;
 var ballX;
 var ballY;
+var ballRadius = 20;
+var ballSpeedX = 10; // right by default
+var ballSpeedY = 5;
 
 function initializeBallCoordinates() {
-    ballX = (canvas.width - ballW) / 2;
-    ballY = (canvas.height - ballH) / 2;
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
 }
 
 window.onload = function(){
     canvas =  document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-
     initializeBallCoordinates();
+    var fps = 288; 
 
-    var fps = 10; 
-    console.log("am intrat")
-    drawRectangle();
-    //setInterval(drawRectangle, 1000 / fps); /// 1000ms = 1s
+    setInterval(showEverything, 1000 / fps); /// 1000ms = 1s
 }
 
-function drawRectangle(){
-    
-    console.log("desenez....");
-    ballX = ballX + 10;
-    console.log(ballX, ballY)
-    canvasContext.fillStyle = 'white'
+function showEverything(){
+    drawElements();
+    recalculateBallCoordinates();
+}
+
+function recalculateBallCoordinates(){
+    ballX = ballX + ballSpeedX;
+    ballY = ballY + ballSpeedY;
+    if (ballX + ballRadius / 2 > canvas.width) {
+        direction = -(ballSpeedX / Math.abs(ballSpeedX));
+        ballSpeedX = Math.floor((Math.random() * 5) + 5)
+        ballSpeedX *= direction;
+        ballX = canvas.width - ballRadius / 2;
+    }
+    if (ballX - ballRadius / 2 < 0) {
+        ballX = ballRadius;
+        direction = -(ballSpeedX / Math.abs(ballSpeedX));
+        ballSpeedX = Math.floor((Math.random() * 5) + 5)
+        ballSpeedX *= direction;
+    }
+    if(ballY + ballRadius / 2 > canvas.height){
+        direction = -(ballSpeedY / Math.abs(ballSpeedY));
+        ballSpeedY = Math.floor((Math.random() * 5) + 5)
+        ballSpeedY *= direction;
+        ballY = canvas.height - ballRadius / 2;
+    }
+    if(ballY - ballRadius / 2  < 0){
+        direction = -(ballSpeedY / Math.abs(ballSpeedY));
+        ballSpeedY = Math.floor((Math.random() * 5) + 5)
+        ballSpeedY *= direction;
+        ballY = ballRadius / 2;
+    }
+}
+
+function drawElements(){
+    drawBackground();
+    console.log(ballX, ballY);
+    colorCircle(ballX, ballY, ballRadius, 'white')
+}
+
+function drawBackground(){
+    canvasContext.fillStyle = 'black'
     canvasContext.fillRect(0, 0, canvas.width, canvas.height)
-    canvasContext.fillStyle = 'red';
-    //canvasContext.fillRect(ballX, ballY, ballW, ballH);
-    canvasContext.arc(ballX, ballY, 20, 0, 2 * Math.PI);
-    if (ballX >= 800)
-        ballX = -50;
+}
+
+function colorCircle(centerX, centerY, radius, color){
     canvasContext.beginPath();
-    canvasContext.arc(100, 75, 50, 0, 2 * Math.PI);
-    canvasContext.stroke();
+    canvasContext.fillStyle = color
+    canvasContext.arc(centerX, centerY, radius, 0, 2 * Math.PI)
+    canvasContext.fill();
 }
 
